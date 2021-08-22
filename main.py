04ed8dfbd32e863be7a5f1947b5596423a9d7cfd
bdvysg <bdvysg@gmail.com>
2021-08-22 20:35:08 +0300

    update main.py

--- main.py
+++ main.py
@@ -2,7 +2,6 @@ from keyboard import add_hotkey, wait
 from pyperclip import paste
 from requests import post
 from json import loads
-from os import system
 
 
 def translate_word(word: str):
@@ -19,16 +18,8 @@ def write_in_file(words: tuple):
     try:
         with open('C:/Users/Admin/Desktop/code/learn_words_with_python/words.txt', 'a') as file:
             file.write(words[0] + ' - ' + words[1] + '\n')
-        
-        git_update()
     except Exception as ex:
         print(ex)
-
-def git_update():
-    system('cd C://Users//Admin//Desktop//code//learn_words_with_python//')
-    system('git add .')
-    system('git commit -m "update words.txt"')
-    system('git push')
 
 
 
